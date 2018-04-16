9416929b9916c326c0ea69fa99ecd47960f391f5
Bogdan Prodan <Bogdan.Prodan@eleks.com>
2018-04-16 17:46:35 +0300

    Fixed marshal/unmarshal identity

--- identity.go
+++ identity.go
@@ -22,12 +22,72 @@ type Identity struct {
 
 // MarshalBinary encodes identity into a binary form and returns the result
 func (i *Identity) MarshalBinary() (data []byte, err error) {
-	return json.Marshal(i)
+	var pk, cert string
+	switch i.PrivateKey.(type) {
+	case *ecdsa.PrivateKey:
+		cast := i.PrivateKey.(*ecdsa.PrivateKey)
+		b, err := x509.MarshalECPrivateKey(cast)
+		if err != nil {
+			return nil, err
+		}
+		block := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: b})
+		pk = base64.RawStdEncoding.EncodeToString(block)
+
+	default:
+		return nil, ErrInvalidKeyType
+	}
+
+	cert = base64.RawStdEncoding.EncodeToString(i.Certificate.Raw)
+	return json.Marshal(map[string]string{"cert": cert, "pk": pk, "mspid": i.MspId})
 }
 
 // UnmarshalBinary unmarshal a binary representation of identity
 func (i *Identity) UnmarshalBinary(data []byte) error {
-	return json.Unmarshal(data, i)
+	var raw map[string]string
+	if err := json.Unmarshal([]byte(data), &raw); err != nil {
+		return err
+	}
+	// check do we have all keys
+	if _, ok := raw["cert"]; !ok || len(raw["cert"]) < 1 {
+		return ErrInvalidDataForParcelIdentity
+	}
+	if _, ok := raw["pk"]; !ok || len(raw["pk"]) < 1 {
+		return ErrInvalidDataForParcelIdentity
+	}
+
+	certRaw, err := base64.RawStdEncoding.DecodeString(raw["cert"])
+	if err != nil {
+		return err
+	}
+	cert, err := x509.ParseCertificate(certRaw)
+	if err != nil {
+		return err
+	}
+
+	keyRaw, err := base64.RawStdEncoding.DecodeString(raw["pk"])
+	if err != nil {
+		return err
+	}
+	keyPem, _ := pem.Decode(keyRaw)
+	if keyPem == nil {
+		return ErrInvalidDataForParcelIdentity
+	}
+	var pk interface{}
+	switch keyPem.Type {
+	case "EC PRIVATE KEY":
+		pk, err = x509.ParseECPrivateKey(keyPem.Bytes)
+		if err != nil {
+			return ErrInvalidDataForParcelIdentity
+		}
+	default:
+		return ErrInvalidDataForParcelIdentity
+	}
+
+	i.Certificate = cert
+	i.PrivateKey = pk
+	i.MspId = raw["mspid"]
+
+	return nil
 }
 
 // EnrollmentId get enrollment id from certificate
@@ -52,80 +112,6 @@ func (i *Identity) ToPem() ([]byte, []byte, error) {
 	default:
 		return nil, nil, ErrInvalidKeyType
 	}
-}
-
-// MarshalIdentity marshal identity to string
-func MarshalIdentity(i *Identity) (string, error) {
-
-	var pk, cert string
-	switch i.PrivateKey.(type) {
-	case *ecdsa.PrivateKey:
-		cast := i.PrivateKey.(*ecdsa.PrivateKey)
-		b, err := x509.MarshalECPrivateKey(cast)
-		if err != nil {
-			return "", err
-		}
-		block := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: b})
-		pk = base64.RawStdEncoding.EncodeToString(block)
-
-	default:
-		return "", ErrInvalidKeyType
-	}
-
-	cert = base64.RawStdEncoding.EncodeToString(i.Certificate.Raw)
-	str, err := json.Marshal(map[string]string{"cert": cert, "pk": pk, "mspid": i.MspId})
-	if err != nil {
-		return "", err
-	}
-
-	return string(str), nil
-}
-
-// UnmarshalIdentity unmarshal identity from string
-func UnmarshalIdentity(data string) (*Identity, error) {
-	var raw map[string]string
-	if err := json.Unmarshal([]byte(data), &raw); err != nil {
-		return nil, err
-	}
-	// check do we have all keys
-	if _, ok := raw["cert"]; !ok || len(raw["cert"]) < 1 {
-		return nil, ErrInvalidDataForParcelIdentity
-	}
-	if _, ok := raw["pk"]; !ok || len(raw["pk"]) < 1 {
-		return nil, ErrInvalidDataForParcelIdentity
-	}
-
-	certRaw, err := base64.RawStdEncoding.DecodeString(raw["cert"])
-	if err != nil {
-		return nil, err
-	}
-	cert, err := x509.ParseCertificate(certRaw)
-	if err != nil {
-		return nil, err
-	}
-
-	keyRaw, err := base64.RawStdEncoding.DecodeString(raw["pk"])
-	if err != nil {
-		return nil, err
-	}
-	keyPem, _ := pem.Decode(keyRaw)
-	if keyPem == nil {
-		return nil, ErrInvalidDataForParcelIdentity
-	}
-	var pk interface{}
-	switch keyPem.Type {
-	case "EC PRIVATE KEY":
-		pk, err = x509.ParseECPrivateKey(keyPem.Bytes)
-		if err != nil {
-			return nil, ErrInvalidDataForParcelIdentity
-		}
-	default:
-		return nil, ErrInvalidDataForParcelIdentity
-	}
-
-	identity := &Identity{Certificate: cert, PrivateKey: pk, MspId: raw["mspid"]}
-	return identity, nil
-
 }
 
 // LoadCertFromFile read public key (pk) and private/secret kye (sk) from file system and return new Identity
